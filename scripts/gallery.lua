96cfca4b70189afcc8f0cbf893e71b7198ec1669
Olivier Perret <Olivier.Perret@mailbox.org>
2018-01-01 14:08:18 +0100

    keep sha code in a separate scope

--- scripts/gallery.lua
+++ scripts/gallery.lua
@@ -38,170 +38,173 @@ opts.thumbs_dir = string.gsub(opts.thumbs_dir, "^~", os.getenv("HOME") or "~")
 --The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 --THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-local band, rrotate, bxor, rshift, bnot
-if bit32 then
-    band, rrotate, bxor, rshift, bnot =  bit32.band, bit32.rrotate, bit32.bxor, bit32.rshift, bit32.bnot
-else
-    -- lua implementation of bit32 from https://www.snpedia.com/extensions/Scribunto/engines/LuaCommon/lualib/bit32.lua
-    -- licensed under MIT too
-    bnot = function(x)
-        x = math.floor(tonumber(x)) % 0x100000000
-        return ( -x - 1 ) % 0x100000000
-    end
-    local logic_and = {
-        [0] = { [0] = 0, 0, 0, 0},
-        [1] = { [0] = 0, 1, 0, 1},
-        [2] = { [0] = 0, 0, 2, 2},
-        [3] = { [0] = 0, 1, 2, 3},
-    }
-    local logic_xor = {
-        [0] = { [0] = 0, 1, 2, 3},
-        [1] = { [0] = 1, 0, 3, 2},
-        [2] = { [0] = 2, 3, 0, 1},
-        [3] = { [0] = 3, 2, 1, 0},
-    }
-    local function comb( name, args, nargs, s, t )
-        for i = 1, nargs do
-            args[i] = math.floor(tonumber(args[i])) % 0x100000000
+local sha256
+do
+    local band, rrotate, bxor, rshift, bnot
+    if bit32 then
+        band, rrotate, bxor, rshift, bnot =  bit32.band, bit32.rrotate, bit32.bxor, bit32.rshift, bit32.bnot
+    else
+        -- lua implementation of bit32 from https://www.snpedia.com/extensions/Scribunto/engines/LuaCommon/lualib/bit32.lua
+        -- licensed under MIT too
+        bnot = function(x)
+            x = math.floor(tonumber(x)) % 0x100000000
+            return ( -x - 1 ) % 0x100000000
         end
-        local pow = 1
-        local ret = 0
-        for b = 0, 31, 2 do
-            local c = s
+        local logic_and = {
+            [0] = { [0] = 0, 0, 0, 0},
+            [1] = { [0] = 0, 1, 0, 1},
+            [2] = { [0] = 0, 0, 2, 2},
+            [3] = { [0] = 0, 1, 2, 3},
+        }
+        local logic_xor = {
+            [0] = { [0] = 0, 1, 2, 3},
+            [1] = { [0] = 1, 0, 3, 2},
+            [2] = { [0] = 2, 3, 0, 1},
+            [3] = { [0] = 3, 2, 1, 0},
+        }
+        local function comb( name, args, nargs, s, t )
             for i = 1, nargs do
-                c = t[c][args[i] % 4]
-                args[i] = math.floor( args[i] / 4 )
+                args[i] = math.floor(tonumber(args[i])) % 0x100000000
             end
-            ret = ret + c * pow
-            pow = pow * 4
+            local pow = 1
+            local ret = 0
+            for b = 0, 31, 2 do
+                local c = s
+                for i = 1, nargs do
+                    c = t[c][args[i] % 4]
+                    args[i] = math.floor( args[i] / 4 )
+                end
+                ret = ret + c * pow
+                pow = pow * 4
+            end
+            return ret
         end
-        return ret
+        band = function( ... )
+            return comb( 'band', { ... }, select( '#', ... ), 3, logic_and )
+        end
+        bxor = function( ... )
+            return comb( 'bxor', { ... }, select( '#', ... ), 0, logic_xor )
+        end
+        rshift = function(x, disp)
+            x = math.floor(tonumber(x)) % 0x100000000
+            disp = math.floor(tonumber(disp))
+            disp = math.min( math.max( -32, disp ), 32)
+            return math.floor( x / 2^disp ) % 0x100000000
+        end
+        rrotate = function(x, disp)
+            x = math.floor(tonumber(x)) % 0x100000000
+            disp = -math.floor(tonumber(disp)) % 32
+            local x = x * 2^disp
+            return ( x % 0x100000000 ) + math.floor( x / 0x100000000 )
+        end
     end
-    band = function( ... )
-        return comb( 'band', { ... }, select( '#', ... ), 3, logic_and )
+    local k = {
+       0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+       0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+       0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+       0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+       0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+       0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+       0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+       0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+       0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+       0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+       0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+       0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+       0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+       0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+       0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+       0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    }
+    local function str2hexa(s)
+        return string.gsub(s, ".", function(c) return string.format("%02x", string.byte(c)) end)
     end
-    bxor = function( ... )
-        return comb( 'bxor', { ... }, select( '#', ... ), 0, logic_xor )
+    local function num2s(l, n)
+        local s = ""
+        for i = 1, n do
+            local rem = l % 256
+            s = string.char(rem) .. s
+            l = (l - rem) / 256
+        end
+        return s
     end
-    rshift = function(x, disp)
-        x = math.floor(tonumber(x)) % 0x100000000
-        disp = math.floor(tonumber(disp))
-        disp = math.min( math.max( -32, disp ), 32)
-        return math.floor( x / 2^disp ) % 0x100000000
+    local function s232num(s, i)
+        local n = 0
+        for i = i, i + 3 do
+            n = n*256 + string.byte(s, i)
+        end
+        return n
     end
-    rrotate = function(x, disp)
-        x = math.floor(tonumber(x)) % 0x100000000
-        disp = -math.floor(tonumber(disp)) % 32
-        local x = x * 2^disp
-        return ( x % 0x100000000 ) + math.floor( x / 0x100000000 )
+    local function preproc(msg, len)
+        local extra = -(len + 1 + 8) % 64
+        len = num2s(8 * len, 8)
+        msg = msg .. "\128" .. string.rep("\0", extra) .. len
+        return msg
     end
-end
-local k = {
-   0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-   0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-   0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-   0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-   0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-   0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-   0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-   0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-   0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-}
-function str2hexa (s)
-    return string.gsub(s, ".", function(c) return string.format("%02x", string.byte(c)) end)
-end
-function num2s (l, n)
-    local s = ""
-    for i = 1, n do
-        local rem = l % 256
-        s = string.char(rem) .. s
-        l = (l - rem) / 256
+    local function initH256(H)
+        H[1] = 0x6a09e667
+        H[2] = 0xbb67ae85
+        H[3] = 0x3c6ef372
+        H[4] = 0xa54ff53a
+        H[5] = 0x510e527f
+        H[6] = 0x9b05688c
+        H[7] = 0x1f83d9ab
+        H[8] = 0x5be0cd19
+        return H
     end
-    return s
-end
-function s232num (s, i)
-    local n = 0
-    for i = i, i + 3 do
-        n = n*256 + string.byte(s, i)
+    local function digestblock(msg, i, H)
+        local w = {}
+        for j = 1, 16 do
+            w[j] = s232num(msg, i + (j - 1)*4)
+        end
+        for j = 17, 64 do
+            local v = w[j - 15]
+            local s0 = bxor(rrotate(v, 7), rrotate(v, 18), rshift(v, 3))
+            v = w[j - 2]
+            local s1 = bxor(rrotate(v, 17), rrotate(v, 19), rshift(v, 10))
+            w[j] = w[j - 16] + s0 + w[j - 7] + s1
+        end
+        local a, b, c, d, e, f, g, h =
+            H[1], H[2], H[3], H[4], H[5], H[6], H[7], H[8]
+        for i = 1, 64 do
+            local s0 = bxor(rrotate(a, 2), rrotate(a, 13), rrotate(a, 22))
+            local maj = bxor(band(a, b), band(a, c), band(b, c))
+            local t2 = s0 + maj
+            local s1 = bxor(rrotate(e, 6), rrotate(e, 11), rrotate(e, 25))
+            local ch = bxor (band(e, f), band(bnot(e), g))
+            local t1 = h + s1 + ch + k[i] + w[i]
+            h = g
+            g = f
+            f = e
+            e = d + t1
+            d = c
+            c = b
+            b = a
+            a = t1 + t2
+        end
+        H[1] = band(H[1] + a)
+        H[2] = band(H[2] + b)
+        H[3] = band(H[3] + c)
+        H[4] = band(H[4] + d)
+        H[5] = band(H[5] + e)
+        H[6] = band(H[6] + f)
+        H[7] = band(H[7] + g)
+        H[8] = band(H[8] + h)
     end
-    return n
-end
-function preproc (msg, len)
-    local extra = -(len + 1 + 8) % 64
-    len = num2s(8 * len, 8)
-    msg = msg .. "\128" .. string.rep("\0", extra) .. len
-    return msg
-end
-local function initH256 (H)
-    H[1] = 0x6a09e667
-    H[2] = 0xbb67ae85
-    H[3] = 0x3c6ef372
-    H[4] = 0xa54ff53a
-    H[5] = 0x510e527f
-    H[6] = 0x9b05688c
-    H[7] = 0x1f83d9ab
-    H[8] = 0x5be0cd19
-    return H
-end
-local function digestblock (msg, i, H)
-    local w = {}
-    for j = 1, 16 do
-        w[j] = s232num(msg, i + (j - 1)*4)
+    local function finalresult256(H)
+        return
+            str2hexa(num2s(H[1], 4)..num2s(H[2], 4)..num2s(H[3], 4)..num2s(H[4], 4)..
+                     num2s(H[5], 4)..num2s(H[6], 4)..num2s(H[7], 4)..num2s(H[8], 4))
     end
-    for j = 17, 64 do
-        local v = w[j - 15]
-        local s0 = bxor(rrotate(v, 7), rrotate(v, 18), rshift(v, 3))
-        v = w[j - 2]
-        local s1 = bxor(rrotate(v, 17), rrotate(v, 19), rshift(v, 10))
-        w[j] = w[j - 16] + s0 + w[j - 7] + s1
+    local HH = {}
+    sha256 = function(msg)
+        msg = preproc(msg, #msg)
+        local H = initH256(HH)
+        for i = 1, #msg, 64 do
+            digestblock(msg, i, H)
+        end
+        return finalresult256(H)
     end
-    local a, b, c, d, e, f, g, h =
-        H[1], H[2], H[3], H[4], H[5], H[6], H[7], H[8]
-    for i = 1, 64 do
-        local s0 = bxor(rrotate(a, 2), rrotate(a, 13), rrotate(a, 22))
-        local maj = bxor(band(a, b), band(a, c), band(b, c))
-        local t2 = s0 + maj
-        local s1 = bxor(rrotate(e, 6), rrotate(e, 11), rrotate(e, 25))
-        local ch = bxor (band(e, f), band(bnot(e), g))
-        local t1 = h + s1 + ch + k[i] + w[i]
-        h = g
-        g = f
-        f = e
-        e = d + t1
-        d = c
-        c = b
-        b = a
-        a = t1 + t2
-    end
-    H[1] = band(H[1] + a)
-    H[2] = band(H[2] + b)
-    H[3] = band(H[3] + c)
-    H[4] = band(H[4] + d)
-    H[5] = band(H[5] + e)
-    H[6] = band(H[6] + f)
-    H[7] = band(H[7] + g)
-    H[8] = band(H[8] + h)
-end
-function finalresult256 (H)
-    return
-        str2hexa(num2s(H[1], 4)..num2s(H[2], 4)..num2s(H[3], 4)..num2s(H[4], 4)..
-                 num2s(H[5], 4)..num2s(H[6], 4)..num2s(H[7], 4)..num2s(H[8], 4))
-end
-local HH = {}
-function sha256(msg)
-    msg = preproc(msg, #msg)
-    local H = initH256(HH)
-    for i = 1, #msg, 64 do
-    digestblock(msg, i, H)
-    end
-    return finalresult256(H)
 end
 
 -- end of sha code
